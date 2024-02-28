28f0684ee270a1a80612c60d0e4f065a4641094a
Jeevan Reddy <jeevanreddy05462@gmail.com>
2024-02-28 21:34:40 +0530

    baselined-UI applications

---
+++ packages/user-app/app/login/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { FC } from "react";
+
+interface LoginProps {}
+
+const Login: FC<LoginProps> = () => {
+  return (
+    <>
+      <h4>Login Page</h4>
+    </>
+  );
+};
+
+export default Login;
